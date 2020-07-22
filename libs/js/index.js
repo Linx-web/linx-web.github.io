5f38c2fbd5c80e35ce626c7cfb97aaf36bcd375d
linx-web <616381944@qq.com>
2020-07-22 18:12:51 +0800

    登录旋转木马页面跳转

--- libs/js/index.js
+++ libs/js/index.js
@@ -1,4 +1,5 @@
 $(function () {
+    //大盒子鼠标移入移出波纹效果
     $('.box').on({
         'mouseenter': function (e) {
             let x = e.pageX - $(this).offset().left;
@@ -19,13 +20,14 @@ $(function () {
             $('.box').removeClass('in').addClass('out');
         }
     });
+    //输入框焦点事件,文字升降
     $('.userName,.password').on({
         'focus': function () {
             if ($(this).hasClass('userName')) {
                 $('form i.name').css('top', 35);
             } else {
                 $('form i.pas').css('top', 115);
-            }
+            };
         },
         'blur': function () {
             if ($(this).val().length == 0) {
@@ -33,8 +35,16 @@ $(function () {
                     $('form i.name').css('top', '');
                 } else {
                     $('form i.pas').css('top', '');
-                }
-            }
+                };
+            };
         }
+    });
+    //登录验证
+    $('#login').on('click', function (e) {
+        //阻止表单按钮默认提交事件
+        e.preventDefault();
+        if ($('.userName').val() == '陈丽丹' && $('.password').val() == '940604') {
+            window.location.assign('./旋转木马.html');
+        };
     });
 });
